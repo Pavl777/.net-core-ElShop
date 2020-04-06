d7208d4d46b7da91187c4de1dcc84474e3a89bf6
Pavel <pavelsukhina.777@gmail.com>
2020-04-06 15:02:30 +0300

    Add WEB and identity

---
+++ WebAdminEShop/Data/Initial/AdminInitialaizer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdminEShop.Data.Initial
+{
+    public class AdminInitialaizer
+    {
+        public static async Task InitializeAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+          
+            string eMail = "Eshopp@mail.ru";
+            string pass = "234Pavel$$$";
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+
+            if (await userManager.FindByNameAsync(eMail) == null)
+            {
+                IdentityUser admin = new IdentityUser { Email = eMail, UserName = eMail };
+                IdentityResult result = await userManager.CreateAsync(admin, pass);
+                if(result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            
+            }
+        }
+    }
+}
---
+++ WebAdminEShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAdminEShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
